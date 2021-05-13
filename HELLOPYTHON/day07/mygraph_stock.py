607f8b13defc3e14da293646912e37c3bb81f492
Jinny-s <46083003+Jinny-s@users.noreply.github.com>
2021-05-13 19:33:49 +0900

    omok commit

--- HELLOPYTHON/day07/mygraph_stock.py
+++ HELLOPYTHON/day07/mygraph_stock.py
@@ -1,4 +1,3 @@
-from mpl_toolkits import mplot3d
 import numpy as np
 import matplotlib.pyplot as plt
 import pymysql
@@ -6,42 +5,36 @@ import random
 
 conn = pymysql.connect(host='localhost', user='root', password='java', db='python', charset='utf8')
 curs = conn.cursor()
-def getPrices(s_name):
+
+def getPrices(s_code):
     ret = []
-    sql = "select s_price from stock where s_name = '{}' order by crawl_date".format(s_name)
-    
+    sql = "select s_price from stock where s_code = '{}' order by crawl_date".format(s_code)
     curs.execute(sql)
     
     rows = curs.fetchall()
     for row in rows:
         ret.append(row[0])
-        
     return ret
 
-names = []
-sql = "select s_name from stock group by s_code"
+def getCodes():
+    codes = []
+    sql = "select s_code from stock group by s_name"
+    curs.execute(sql)
+    rows = curs.fetchall()
 
-curs.execute(sql)
-rows = curs.fetchall()
+    for row in rows:
+        codes.append(row[0])
+    return codes
 
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 
-for row in rows:
-    names.append(row[0])
-
-for i, name in enumerate(names):
-    r = random.random()
-    g = random.random()
-    b = random.random()
-    color = (r, g, b)
-    
-    a = np.array(getPrices(name))
-    
+for i, code in enumerate(getCodes()):
+    a = np.array(getPrices(code))
     z = a / a[0]
     x = np.array([1,1,1,1,1,1,1,1,1,1])
     y = np.array([0,1,2,3,4,5,6,7,8,9])
-    ax.plot3D(x+i, y, z, c = color)
+    ax.plot3D(x+i, y, z)
 
 conn.close()
 
